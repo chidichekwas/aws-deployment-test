da709448ff2e9530505916c3246cdda050da4848
Chidiebere <chidichekwas@yahoo.com>
2026-02-14 09:13:17 -0600

    model commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask, request, jsonify
+from model import model
+
+app = Flask(__name__)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.json
+    x = data.get("value")
+    result = model.predict(x)
+    return jsonify({"prediction": result})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
---
+++ model.py
@@ -0,0 +1,6 @@
+# model.py
+class SimpleModel:
+    def predict(self, x):
+        return x * 2  # trivial example
+
+model = SimpleModel()
